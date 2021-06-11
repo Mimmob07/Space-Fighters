845b0599852142bacfdacbe5be37393974c9b11a
Mimmo b <mimmo.baluyut@icloud.com>
2021-06-11 08:55:59 -0400

    Created file dedicated to GameState Class

---
+++ gamestate.py
@@ -0,0 +1,14 @@
+
+class GameState:
+    def __init__(self):
+        self.states = ["Main Menu", "Playing Game", "In Game Menu"]
+        self.GAMESTATE = self.states[0]
+
+    def set_gamestate(self, state):
+        self.GAMESTATE = self.states[state]
+
+    def get_gamestate(self):
+        return self.GAMESTATE
+
+    def get_states(self):
+        return self.states
