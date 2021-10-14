f6e0ead77dc04d6b26beea6a6a231a8021455a1b
Mimmo b <mimmo.baluyut@icloud.com>
2021-10-14 10:02:44 -0400

    Added movement to spaceship

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ RPCThread.start()
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 bg = pygame.image.load("assets/bg.png")
+spaceship1 = pygame.image.load("assets/spaceship.png")
+spaceship2 = pygame.image.load("assets/spaceship2.png")
 FPS = 60
 pygame.init()
 clock = pygame.time.Clock()
+alive = True
+global x
+x = (WIDTH * 0.45)
+y = (HEIGHT * 0.8)
+x_change = 0
+y_change = 0
+car_speed = 0
 
 
 def button(window, position, text):
@@ -37,41 +46,72 @@ def draw_window():
     global b3
     b3 = button(WIN, (50, 260), "Quit")
     if GSInstance.get_gamestate() == GSInstance.get_states()[1]:
-        draw_game()
+        draw_game(x, y)
         pygame.display.update()
     else:
         pygame.display.update()
 
 
-def draw_game():
+def draw_game(x, y):
     WIN.blit(bg, (0, 0))
     font = pygame.font.SysFont("Arial", 20)
     localfps = str(clock.get_fps())
     localfps = localfps.split(".")
     text_render = font.render(localfps[0], True, (255, 255, 255))
+    WIN.blit(spaceship1, (x, y))
     WIN.blit(text_render, (0, 0))
 
 
-def main():
-    run = True
-    while run:
-        clock.tick(FPS)
-        draw_window()
-        pygame.display.set_caption("Space Fighters - " + GSInstance.get_gamestate())
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+def draw_settings():
+    WIN.blit(bg, (0, 0))
+    font = pygame.font.SysFont("impact", 20)
+
+
+# def main():
+run = True
+while run:
+    clock.tick(FPS)
+    draw_window()
+    pygame.display.set_caption("Space Fighters - " + GSInstance.get_gamestate())
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if b1.collidepoint(pygame.mouse.get_pos()):
+                GSInstance.set_gamestate(1)
+                draw_window()
+            elif b2.collidepoint(pygame.mouse.get_pos()):
+                print("Settings Button Clicked")
+            elif b3.collidepoint(pygame.mouse.get_pos()):
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if b1.collidepoint(pygame.mouse.get_pos()):
-                    GSInstance.set_gamestate(1)
-                    draw_window()
-                elif b2.collidepoint(pygame.mouse.get_pos()):
-                    print("Settings Button Clicked")
-                elif b3.collidepoint(pygame.mouse.get_pos()):
-                    run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                GSInstance.set_gamestate(0)
+                draw_window()
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+            elif event.key == pygame.K_UP:
+                y_change = -5
+            elif event.key == pygame.K_DOWN:
+                y_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_change = 0
+    y += y_change
+    if y > 420:
+        y = 420
+    if y < -20:
+        y = -20
+    x += x_change
+    if x < -15:
+        x = -15
+    if x > 820:
+        x = 820
+pygame.quit()
 
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
